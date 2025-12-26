548130333a849d6869262cdb35cd08faec7bcd50
대타출동 <166669923+DaeTaChulDong@users.noreply.github.com>
2025-12-27 02:16:47 +0900

    Create 1_library.py

---
+++ 1_library.py
@@ -0,0 +1,7 @@
+#셀1: 라이브러리 설치
+!pip install google-api-python-client oauth2client \
+             streamlit pyngrok openai yt-dlp chromadb \
+             torch transformers accelerate librosa opencv-python-headless moviepy tqdm
+!sudo apt-get update && sudo apt-get install -y ffmpeg
+# PyTorch, HuggingFace 라이브러리, OpenAI
+!pip install -q streamlit openai moviepy opencv-python-headless pandas numpy torch torchvision torchaudio transformers accelerate
